254c156d29300ca24a0537e0bd5a3f24cf181172
Janson-L <52344189+Janson-L@users.noreply.github.com>
2019-10-17 22:22:18 +0800

    Login.php is connected to database
    Working login page

--- login.php
+++ login.php
@@ -13,37 +13,49 @@
         <h1>UTeM Student Tutor System</h1>
         
         <?php
-
+       
         $loginCredentialCheck = true;
         $loginStatus=false;
-
+        $i=0;
+        $j=0;
         $out="";
 
-        define('USERID','Ken');
-        define('PASS','abc');
-        
+        $dbc=mysqli_connect('localhost','root','','utem_student_tutor_system') or die("Connection not established");
+
         if(isset($_GET['userId'])){
             $userId=$_GET['userId'];
+            $query="SELECT userid FROM LoginCredentials WHERE UserID='$userId';";
+            $result=mysqli_query($dbc, $query) or die("Query Failed");
+            $userIdDb=mysqli_fetch_assoc($result);
         }
         else{
+           
+            if($i>=1){
             $loginCredentialCheck = false;
             $out .="Missing Username ";
+            }
         }
 
         if(isset($_GET['pass'])){
         $pass=$_GET['pass'];
+        $query="SELECT password FROM LoginCredentials WHERE UserID='$userId';";
+        $result=mysqli_query($dbc, $query) or die("Query Failed");
+        $passDb=mysqli_fetch_assoc($result);
         }
+        
         else{
             $loginCredentialCheck = false;
+            if($j>=1){
             $out .="Missing Password ";
+            }
         }
         
         if ($loginCredentialCheck==true){
-            if(($userId == USERID) && ($pass == PASS)){
+            if(($userId === $userIdDb['userid']) && ($pass === $passDb['password'])){
                 $out .="You are now logged in";  
                 $loginStatus=true; 
-            }else{
-                $out .="Incorrect Credentials. Contact administrator for further assistance";
+            }else{       
+                $out .="Incorrect Credentials. Please try again or contact administrator for further assistance";
             }
         }
 
