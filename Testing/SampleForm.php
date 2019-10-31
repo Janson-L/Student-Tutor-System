94b92fbcfa7c63740f973ee243e7b73abed4fd15
Janson-L <52344189+Janson-L@users.noreply.github.com>
2019-10-31 14:51:42 +0800

    Update SampleForm.php
    -Sticky Data implemented
    -Basic HTML form validation is done
    -Next step should be creating a new file in master to implement database connection

--- Testing/SampleForm.php
+++ Testing/SampleForm.php
@@ -12,59 +12,71 @@
             $dbc=mysqli_connect('localhost','root','','utem_student_tutor_system') or die("Connection not established"); //Register and change to a non root user
             $formCheck=true;
 
-            $out=""; 
+            $out="";
+            $userType=""; 
             $userName="";
             $matrixNo="";
             $phoneNo="";
             $userID=""; //need to be connected to database to fetch last userID, do some processing to generate new ID
+            $successRegistration=false;
+
+            if (isset($_GET['userType'])){
+            $userType=$_GET['userType'];
+            }
 
             if(isset($_GET['userName'])){
                 $userName=$_GET['userName'];
             }
 
-            else{
-                $formCheck=false;
-                $out .="Missing Name ";
-            }
-
             if(isset($_GET['matrixNo'])){
-                $userName=$_GET['matrixNo'];
+                $matrixNo=$_GET['matrixNo'];
             }
-
-            else{
-                $formCheck=false;
-                $out .="Missing Matrix No ";
-            }
-
+          
             if(isset($_GET['phoneNo'])){
-                $userName=$_GET['phoneNo'];
+                $phoneNo=$_GET['phoneNo'];
             }
 
-            else{
-                $formCheck=false;
-                $out .="Missing Phone No ";
+            if(isset($_GET['pass'])&&isset($_GET['passRetype']))
+            {
+                if($_GET['pass']!=$_GET['passRetype'])
+                {
+                    $formCheck=false;
+                    $out .="Incorrect Password. Please make sure password is the same.";
+                }     
+                else
+                {
+                    $successRegistration=true;
+                }
             }
         ?>
 
         <form action='SampleForm.php' method='GET'>
         <label>User Type</label>
         <select name='userType' required>
-        <option value='student'>Student</option>
-        <option value='tutor'>Tutor</option>
+        <option <?php if($userType=="student") echo 'selected="selected"'; ?>value='student'>Student</option>
+        <option <?php if($userType=="tutor") echo 'selected="selected"'; ?>value='tutor'>Tutor</option>
+    
+
         </select><br>
 
-        <label>Name: </label><input type='text' name='userName' value='<?php echo $userName ?>' required><br>
-        <label>Matrix No: </label><input type='text' name='matrixNo' value='<?php echo $matrixNo ?>' required ><br>
-        <label>Phone No: </label><input type='text' name='phoneNo' value='<?php echo $phoneNo ?>' required ><br>
+        <label>Name: </label><input type='text' name='userName' value='<?php echo $userName ?>' pattern="[A-Za-z\s\W]{5,30}" required><br>
+        <label>Matrix No: </label><input type='text' name='matrixNo' value='<?php echo $matrixNo ?>' pattern="[A-Z]{1}[0-9]{9}" required ><br>
+        <label>Phone No: </label><input type='text' name='phoneNo' value='<?php echo $phoneNo ?>' pattern="[0-9]{10,11}" required ><br>
         <label>UserID: </label><input type='text' name='userID' value='<?php echo $userID ?>'readonly><br>
         <label>Password: </label><input type='password' name='pass' required><br>
-        <label>Retype Password: </label><input type='password' name='passRetype' required><br>
+        <label>Retype Password: </label><input type='password' name='passRetype' required><br>   
         <input type='submit' value='Submit Form'><br>
         </form>
 
-
         <?php
-            echo "<h5>$out</h5>";
+            if($formCheck==false){
+                echo "<h5>$out</h5>";
+            }
+
+            if($successRegistration==true)
+            {
+                echo "<h5>Successful Registration</h5>";
+            }
         ?>
     </body>
 </html>
