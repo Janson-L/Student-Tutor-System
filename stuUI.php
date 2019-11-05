ce0956599275b536fd8b7523892a545362ba2e31
Janson-L <52344189+Janson-L@users.noreply.github.com>
2019-11-05 13:47:00 +0800

    Done draft for button page redirect
    -Template for redirecting to respective page is done
    -Remember to destroy session

--- stuUI.php
+++ stuUI.php
@@ -2,5 +2,24 @@
     SESSION_START(); 
     //$_SESSION['loginUser']
     //$_SESSION['userClass']
+    $out="";
+    $out .= $_SESSION['loginUser'];
+    $out .= $_SESSION['userClass'];
 ?>
-<h2>Student UI</h2>
+
+<html>
+    <head>
+        <style>
+        </style>
+    </head>
+    <body>
+        <h2>Student UI</h2>
+        <form action='studentSessionSignUp.php' method='GET'>
+            <button type='Submit'>Sign up for tutoring session</button>
+        </form>
+    </body>
+</html>
+
+<?php
+    echo "<h2> $out</h2>";
+?>
