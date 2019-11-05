8362ec34d0eba3063b3b26a05fbb0caa4fa23e3b
Janson-L <52344189+Janson-L@users.noreply.github.com>
2019-11-05 23:09:06 +0800

    Done TutorUI template

--- tutUI.php
+++ tutUI.php
@@ -2,5 +2,26 @@
     SESSION_START(); 
     //$_SESSION['loginUser']
     //$_SESSION['userClass']
+    $out="";
+    $out .= $_SESSION['loginUser'];
+    $out .= $_SESSION['userClass'];
+    echo "<h6> $out<h6>";
 ?>
-<h2>Tutor UI</h2>
+    <html>
+        <head>
+            <style>
+            </style>
+        </head>
+        <body>
+            <h2>Student UI</h2>
+            <form action='tutNewSession.php' method='GET'>
+                <button type='Submit'>Open a tutor session</button> <br>
+            </form>
+            <form action='tutShowTimetable.php' method='GET'>
+                <button type='Submit'>Show registered tutoring session</button> <br>
+            </form>
+            <form action='logOut.php' method='GET'>
+                <button type='Submit'>Log Out</button> <br>
+            </form>
+        </body>
+    </html>
