8e886b52dce9d19a3c911577edbeca3204d1bbc9
Janson-L <52344189+Janson-L@users.noreply.github.com>
2019-11-22 14:16:16 +0800

    Changed from description to subject code
    -changes are applied to the DB as (optional)
    -start to include PHP codes

--- tutNewSession.php
+++ tutNewSession.php
@@ -17,38 +17,37 @@
             $dbc=mysqli_connect('localhost','root','','utem_student_tutor_system') or die("Connection not established"); //Register and change to a non root user
             $formCheck=true;
             $topic="";
-            $description="";
+            $Subject Code="";
             $date="";
             $startTime="";
-            $endTime="";
-            
+            $endTime="";  
         ?>
 
         <form action='tutNewSession.php' method='GET'>
-        <label>Topic: </label><input type='text' name='topic' value='<?php echo $topic ?>' required size="30"><br>
-        <label>Description: </label><textarea name="description" rows="5" cols="25" size="50" value='<?php echo $topic ?>'></textarea> <br>
-        <label>Date: </label><input type='date' name='date' value='<?php echo $date ?>' required ><br>
-        <label>Start Time:</label> 
-        <select name='startTimeH' required>
-            <?php for($i=0;$i<=9;$i++) echo "<option value=0".$i.">0".$i." </option>";?>
-            <?php for($i=10;$i<=23;$i++) echo "<option value=".$i.">".$i." </option>";?>
-        </select>
-        <select name='startTimeM' required>
-            <?php for($i=0;$i<=9;$i++) echo "<option value=0".$i.">0".$i." </option>";?>
-            <?php for($i=10;$i<=59;$i++) echo "<option value=".$i.">".$i." </option>";?>
-        </select>
-        <br>
-        <label>End Time: </label>
-        <select name='endTimeH' required>
-            <?php for($i=0;$i<=9;$i++) echo "<option value=0".$i.">0".$i." </option>";?>
-            <?php for($i=10;$i<=23;$i++) echo "<option value=".$i.">".$i." </option>";?>
-        </select>
-        <select name='endTimeM' required>
-            <?php for($i=0;$i<=9;$i++) echo "<option value=0".$i.">0".$i." </option>";?>
-            <?php for($i=10;$i<=59;$i++) echo "<option value=".$i.">".$i." </option>";?>
-        </select><br>
-        
-        <input type='submit' value='Add new tutoring session'><br>
+            <label>Topic: </label><input type='text' name='topic' value='<?php echo $topic ?>' required size="30"><br>
+            <label>Subject Code(if applicable): </label><input type='text' name="Subject Code" value='<?php echo $ ?>'></textarea> <br>
+            <label>Date: </label><input type='date' name='date' value='<?php echo $date ?>' required ><br>
+            <label>Start Time:</label> 
+            <select name='startTimeH' required>
+                <?php for($i=0;$i<=9;$i++) echo "<option value=0".$i.">0".$i." </option>";?>
+                <?php for($i=10;$i<=23;$i++) echo "<option value=".$i.">".$i." </option>";?>
+            </select>
+            <select name='startTimeM' required>
+                <?php for($i=0;$i<=9;$i++) echo "<option value=0".$i.">0".$i." </option>";?>
+                <?php for($i=10;$i<=59;$i++) echo "<option value=".$i.">".$i." </option>";?>
+            </select>
+            <br>
+            <label>End Time: </label>
+            <select name='endTimeH' required>
+                <?php for($i=0;$i<=9;$i++) echo "<option value=0".$i.">0".$i." </option>";?>
+                <?php for($i=10;$i<=23;$i++) echo "<option value=".$i.">".$i." </option>";?>
+            </select>
+            <select name='endTimeM' required>
+                <?php for($i=0;$i<=9;$i++) echo "<option value=0".$i.">0".$i." </option>";?>
+                <?php for($i=10;$i<=59;$i++) echo "<option value=".$i.">".$i." </option>";?>
+            </select><br>
+            
+            <input type='submit' value='Add new tutoring session'><br>
         </form>
         
         <?php
