3f4547c6a2a09ac16545595c4078a9e20ae62789
Janson-L <52344189+Janson-L@users.noreply.github.com>
2019-11-23 12:28:18 +0800

    Updated the minute selection
    -now it is on increment of 5 instead of 1 for minute selection to enhance user experience

--- tutNewSession.php
+++ tutNewSession.php
@@ -2,10 +2,12 @@
     SESSION_START(); 
     //$_SESSION['loginUser']
     //$_SESSION['userClass']
-    $out="";
+    $_SESSION['loginUser']="TUT1"; //testing TUT1
+    $_SESSION['userClass']="TUT";
     $out .= $_SESSION['loginUser'];
     $out .= $_SESSION['userClass'];
     echo "<h6> $out<h6>";
+    $out="";
 
     $dbc=mysqli_connect('localhost','root','','utem_student_tutor_system') or die("Connection not established"); //Register and change to a non root user
     $formFilledCorrectly=true;
@@ -98,8 +100,8 @@
                 <?php for($i=10;$i<=23;$i++) echo "<option value=".$i.">".$i." </option>";?>
             </select>
             <select name='startTimeM' required>
-                <?php for($i=0;$i<=9;$i++) echo "<option value=0".$i.">0".$i." </option>";?>
-                <?php for($i=10;$i<=59;$i++) echo "<option value=".$i.">".$i." </option>";?>
+                <?php for($i=0;$i<=9;$i+=5) echo "<option value=0".$i.">0".$i." </option>";?>
+                <?php for($i=10;$i<=59;$i+=5) echo "<option value=".$i.">".$i." </option>";?>
             </select>
             <br>
             <label>End Time: </label>
@@ -108,8 +110,8 @@
                 <?php for($i=10;$i<=23;$i++) echo "<option value=".$i.">".$i." </option>";?>
             </select>
             <select name='endTimeM' required>
-                <?php for($i=0;$i<=9;$i++) echo "<option value=0".$i.">0".$i." </option>";?>
-                <?php for($i=10;$i<=59;$i++) echo "<option value=".$i.">".$i." </option>";?>
+                <?php for($i=0;$i<=9;$i+=5) echo "<option value=0".$i.">0".$i." </option>";?>
+                <?php for($i=10;$i<=59;$i+=5) echo "<option value=".$i.">".$i." </option>";?>
             </select><br>
             
             <input type='submit' value='Add new tutoring session'><br>
