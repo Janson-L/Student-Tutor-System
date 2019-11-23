d39e9530fb417b1f6685ab449f08b4e85dd332b0
Janson-L <52344189+Janson-L@users.noreply.github.com>
2019-11-23 11:29:06 +0800

    Done all the input fields
    -Verified it is the correct format to be fed into DB

--- tutNewSession.php
+++ tutNewSession.php
@@ -6,23 +6,63 @@
     $out .= $_SESSION['loginUser'];
     $out .= $_SESSION['userClass'];
     echo "<h6> $out<h6>";
+
+    $dbc=mysqli_connect('localhost','root','','utem_student_tutor_system') or die("Connection not established"); //Register and change to a non root user
+    $formFilledCorrectly=true;
+    $topic="";
+    $subjectCode="";
+    $date="";
+    $startTime="";
+    $endTime=""; 
+
+    if (isset($_GET['topic'])){
+        $topic=$_GET['topic'];
+    }
+
+    if(isset($_GET['subjectCode'])){
+        $subjectCode=$_GET['subjectCode'];
+    }
+
+    if(isset($_GET['date'])){
+        $date=$_GET['date'];
+    }
+
+    if(isset($_GET['startTimeH'])){
+        $startTime.=$_GET['startTimeH'];
+    }
+
+    if(isset($_GET['startTimeM'])){
+        $startTime.=$_GET['startTimeM'];
+    }
+
+    if(isset($_GET['endTimeH'])){
+        $endTime.=$_GET['endTimeH'];
+    }
+
+    if(isset($_GET['endTimeM'])){
+        $endTime.=$_GET['endTimeM'];
+    }
+
+    if($formFilledCorrectly==true)
+    {
+        $startTime.="00";
+        $endTime.="00";
+        //$query ="INSERT INTO tutoringSession (topic,subjectCode,tutorID,date, startTime, endTime) VALUES('$topic','$matrixNo','$phoneNo','$pass',0,1);";
+        //$result = mysqli_query($dbc, $query) or die("Query Failed $query");
+        $out .="Start Time= $startTime ";
+        $out .="End Time= $endTime ";
+        $out .="Date= $date ";
+
+        echo "$out";
+    }
+
+
 ?>
 
 <html>
     <head>
     </head>
     <body>
-       
-        <?php
-            $dbc=mysqli_connect('localhost','root','','utem_student_tutor_system') or die("Connection not established"); //Register and change to a non root user
-            $formCheck=true;
-            $topic="";
-            $subjectCode="";
-            $date="";
-            $startTime="";
-            $endTime="";  
-        ?>
-
         <form action='tutNewSession.php' method='GET'>
             <label>Topic: </label><input type='text' name='topic' value='<?php echo $topic ?>' required size="30"><br>
             <label>Subject Code(if applicable): </label><input type='text' name="subjectCode" value='<?php echo $subjectCode ?>'></textarea> <br>
