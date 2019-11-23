1f2f8d75e177593303a338170d3e50613997fcf6
Janson-L <52344189+Janson-L@users.noreply.github.com>
2019-11-23 13:20:55 +0800

    Done newSession Form

--- tutNewSession.php
+++ tutNewSession.php
@@ -4,18 +4,25 @@
     //$_SESSION['userClass']
     $_SESSION['loginUser']="TUT1"; //testing TUT1
     $_SESSION['userClass']="TUT";
-    $out .= $_SESSION['loginUser'];
-    $out .= $_SESSION['userClass'];
-    echo "<h6> $out<h6>";
+?>
+<body>
+    <h3>New Tutor Session Form</h3>
+</body>
+<?php
+    $tutorID= $_SESSION['loginUser'];
     $out="";
 
     $dbc=mysqli_connect('localhost','root','','utem_student_tutor_system') or die("Connection not established"); //Register and change to a non root user
-    $formFilledCorrectly=true;
+    $formFilledCorrectly=false;
     $topic="";
     $subjectCode="";
     $date="";
     $startTime="";
     $endTime=""; 
+    $sessionID="";
+    $currentDate="";
+    $currentTime="";
+    $location="";
 
     if (isset($_GET['topic'])){
         $topic=$_GET['topic'];
@@ -47,35 +54,67 @@
         $endTime.="00";
     }
 
-    $currentDate=date('Y-m-d', time());
-    $currentTime= date('His', time());
-    $currentTime+="070000"; //to convert it to Malaysia Time
-    
-    if($date < $currentDate) //to check the selected date with current date
-    {
-        $formFilledCorrectly=false;
-        $out.="Date selected is not a valid date. Please select a date that is either today or later than current date. <br> ";
+    if(isset($_GET['location'])){
+        $location=$_GET['location'];
     }
-    else{
-        if($startTime < $currentTime)
+
+    if(isset($_GET['startTimeH'])&&isset($_GET['startTimeM'])&&isset($_GET['endTimeH'])&&isset($_GET['endTimeM']))
+    {   
+        $formFilledCorrectly=true;
+        $currentDate=date('Y-m-d', time());
+        $currentTime= date('His', time());
+        $currentTime+="070000"; //to convert it to Malaysia Time
+        
+        if($date < $currentDate) //to check the selected date with current date
         {
             $formFilledCorrectly=false;
-            $out.="Time selected is not a valid time. Please select time that is later than current time. <br> ";
+            $out.="Date selected is not a valid date. Please select a date that is either today or later than current date. <br> ";
         }
-        else if(($startTime-$currentTime)<="015900")
-        {
-            $formFilledCorrectly=false;
-            $out.="Tutors are not allowed to add tutor session that starts in less than 2 hours from the current time. <br> ";
-        }
-    }   
+        else{
+            if($date==$currentDate){
+                if($startTime <= $currentTime)
+                {
+                    $formFilledCorrectly=false;
+                    $out.="Time selected is not a valid time. Please select time that is later than current time. <br> ";
+                }
+                else if(($startTime-$currentTime)<="015900")
+                {
+                    $formFilledCorrectly=false;
+                    $out.="Tutors are not allowed to add tutor session that starts in less than 2 hours from the current time. <br> ";
+                }
+            }
+
+            if($startTime>=$endTime)
+            {
+                $formFilledCorrectly=false;
+                $out.="Please select the correct end time. Duration of the session must be at least 5 minutes. <br> ";
+            }
+        }   
+    }
 
     if($formFilledCorrectly==true)
     {
-        //$query ="INSERT INTO tutoringSession (topic,subjectCode,tutorID,date, startTime, endTime) VALUES('$topic','$matrixNo','$phoneNo','$pass',0,1);";
-        //$result = mysqli_query($dbc, $query) or die("Query Failed $query");
-        
+        $query="INSERT INTO tutoringSession (topic,subjectCode,tutorID,date,startTime,endTime,location) VALUES('$topic','$subjectCode','$tutorID','$date','$startTime','$endTime','$location');";
+        $result=mysqli_query($dbc, $query) or die("Query Failed $query");
 
-        
+        $query="SELECT ID FROM tutoringSession ORDER BY ID DESC LIMIT 1;";
+                    $result=mysqli_query($dbc, $query) or die("Query Failed");
+                    $IdDb=mysqli_fetch_assoc($result);
+                    $IdDb=$IdDb['ID'];
+                    $sessionID.="SES";
+
+                    if($IdDb==null){
+                       $id=1;
+                    }
+                    else{
+                        $id=$IdDb;
+                    }
+                    $sessionID.=$id;
+                    
+                    $query="UPDATE tutoringSession SET sessionID='$sessionID' WHERE ID=$IdDb;";
+                    $result = mysqli_query($dbc, $query) or die("Query Failed $query"); 
+
+        $out.="Tutoring session successfully added. SessionID for this tutoring session is $sessionID <br>"; 
     }
 
     $out .="Start Time= $startTime <br>";
@@ -113,6 +152,7 @@
                 <?php for($i=0;$i<=9;$i+=5) echo "<option value=0".$i.">0".$i." </option>";?>
                 <?php for($i=10;$i<=59;$i+=5) echo "<option value=".$i.">".$i." </option>";?>
             </select><br>
+            <label>Location:</label><input type='text' name='location' value='<?php echo $location ?>' required size="20"><br>
             
             <input type='submit' value='Add new tutoring session'><br>
         </form>
