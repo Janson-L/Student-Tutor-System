0656e4ae5a22511f9ae01dbe069813ac453366da
Janson-L <52344189+Janson-L@users.noreply.github.com>
2019-11-24 17:53:55 +0800

    Implemented disappeared register button
    -if student is registered in that session, they are unable to deregister from the session

--- stuSessionSignUp.php
+++ stuSessionSignUp.php
@@ -10,10 +10,11 @@
     $studentID=$_SESSION['loginUser'];
 ?>
 
-<body><h3>10 Newly Added Session </h3></body>
+<body><h3>10 Latest Added Session </h3></body>
 <?php
     $query="SELECT sessionID,topic,subjectCode,date,startTime,endTime,location FROM tutoringsession ORDER BY sessionID DESC LIMIT 10;";
-    $result=mysqli_query($dbc, $query) or die("Query Failed $query");     
+    $result=mysqli_query($dbc, $query) or die("Query Failed $query");  
+    $sessionRegistered=false;   
 ?>
 <table border='1'>
     <tr><td>Session ID</td><td>Topic</td><td>Subject Code</td><td>Date</td><td>Start Time</td><td>End Time</td><td>Duration(Hour(s))</td><td>Location</td><td>Register</td></tr>
@@ -32,6 +33,15 @@
             <td><?php echo $duration; ?></td>
             <td><?php echo $row['location']; ?></td>
             <td>
+                <?php 
+                    $sessionRegistered=false;
+                    $query2="SELECT studentID FROM session_student WHERE sessionID='{$row['sessionID']}';";
+                    $result2=mysqli_query($dbc, $query2) or die("Query Failed $query2");
+                    $stuID=mysqli_fetch_assoc($result2);
+                    if($stuID['studentID']===$_SESSION['loginUser']){
+                    $sessionRegistered=true;
+                    }
+                ?>
                 <input type="text" name="sessionID" value="<?php echo $row['sessionID']; ?>" style="display:none">
                 <input type="text" name="topic" value="<?php echo $row['topic']; ?>" style="display:none">
                 <input type="text" name="subjectCode" value="<?php echo $row['subjectCode']; ?>" style="display:none">
@@ -40,7 +50,9 @@
                 <input type="text" name="endTime" value="<?php echo $row['endTime']; ?>" style="display:none">
                 <input type="text" name="duration" value="<?php echo $duration; ?>" style="display:none">
                 <input type="text" name="location" value="<?php echo $row['locaton']; ?>" style="display:none">
-                <input type="submit" name="submit" value="Register">
+                <?php if ($sessionRegistered!=true) {?>
+                <input type="submit" name="register" value="Register">
+                <?php } ?>
             </td> 
             </form>
         </tr>
@@ -48,7 +60,7 @@
 </table>
 
 <?php
-    if(isset($_GET['submit'])){
+    if(isset($_GET['register'])){
         $query="INSERT INTO session_student (sessionID,studentID) VALUES('{$_GET['sessionID']}','{$_SESSION['loginUser']}');";
         $result=mysqli_query($dbc, $query) or die("Query Failed $query");
     }
