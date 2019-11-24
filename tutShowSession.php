a127b3c90049ca748f55de8485d064916cb6936a
Janson-L <52344189+Janson-L@users.noreply.github.com>
2019-11-24 12:30:26 +0800

    Update tutShowSession.php
    -Added duration calculation
    -Consider adding no. of students column

--- tutShowSession.php
+++ tutShowSession.php
@@ -17,9 +17,10 @@
     $result=mysqli_query($dbc, $query) or die("Query Failed $query");
 
     echo"<table border='1'>";
-    echo"<tr><td>Session ID</td><td>Topic</td><td>Subject Code</td><td>Date</td><td>Start Time</td><td>End Time</td><td>Location</td></tr>\n";
+    echo"<tr><td>Session ID</td><td>Topic</td><td>Subject Code</td><td>Date</td><td>Start Time</td><td>End Time</td><td>Duration(Hour(s))</td><td>Location</td></tr>\n";
     while($row=mysqli_fetch_assoc($result)){
-        echo"<tr><td>{$row['sessionID']}</td><td>{$row['topic']}</td><td>{$row['subjectCode']}</td><td>{$row['date']}</td><td>{$row['startTime']}</td><td>{$row['endTime']}</td><td>{$row['location']}</td></tr>\n";
+        $duration=(strtotime($row['endTime'])-strtotime($row['startTime']))/3600;
+        echo"<tr><td>{$row['sessionID']}</td><td>{$row['topic']}</td><td>{$row['subjectCode']}</td><td>{$row['date']}</td><td>{$row['startTime']}</td><td>{$row['endTime']}</td><td>$duration</td><td>{$row['location']}</td></tr>\n";
     }
     echo"</table>";
 ?>
