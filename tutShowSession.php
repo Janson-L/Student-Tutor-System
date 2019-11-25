b338b18a6cde6c070d11e8a12fe472911bb4e70e
Janson-L <52344189+Janson-L@users.noreply.github.com>
2019-11-25 23:43:09 +0800

    Done showing student list in tutor

--- tutShowSession.php
+++ tutShowSession.php
@@ -1,14 +1,7 @@
 <?php
 SESSION_START();
-$_SESSION['loginUser'] = "TUT1";
-$_SESSION['userClass'] = "TUT";
-$out = "";
-$out .= $_SESSION['loginUser'];
-$out .= $_SESSION['userClass'];
-echo "<h6> $out<h6>";
-?>
-
-<?php
+//$_SESSION['loginUser'] = "STU1";
+//$_SESSION['userClass'] = "STU";
 $out = "";
 $dbc = mysqli_connect('localhost', 'root', '', 'utem_student_tutor_system') or die("Connection not established");
 
@@ -21,7 +14,7 @@ $query = "SELECT sessionID,topic,subjectCode,date,startTime,endTime,location FRO
 $result = mysqli_query($dbc, $query) or die("Query Failed $query");
 
 ?>
-
+<h3>Show Tutor Session</h3>
 <table border='1'>
     <tr>
         <th>Session ID</th>
@@ -32,23 +25,31 @@ $result = mysqli_query($dbc, $query) or die("Query Failed $query");
         <th>End Time</th>
         <th>Duration(Hour(s))</th>
         <th>Location</th>
+        <th>No. of Students</th>
         <th>Show Student List</th>
     </tr>
 
     <tr>
         <?php
         while ($row = mysqli_fetch_assoc($result)) {
-            $durationd = format_time_output(strtotime($row['endTime']) - strtotime($row['startTime'])); ?>
+            $durationd = format_time_output(strtotime($row['endTime']) - strtotime($row['startTime'])); 
+            
+            $query2="SELECT COUNT(studentID) AS noOfStudent FROM session_student WHERE sessionID='{$row['sessionID']}';";
+            $result2=mysqli_query($dbc,$query2) or die("Query Failed $query2");
+            $row2=mysqli_fetch_assoc($result2);
+            ?>
+
 
             <form method='POST'action='tutShowStudents.php'>
-                <td><?php echo "{$row['sessionID']}" ?></td>
-                <td><?php echo "{$row['topic']}" ?></td>
-                <td><?php echo "{$row['subjectCode']}" ?></td>
-                <td><?php echo "{$row['date']}" ?></td>
-                <td><?php echo "{$row['startTime']}" ?></td>
-                <td><?php echo "{$row['endTime']}" ?></td>
-                <td><?php echo "$durationd" ?></td>
-                <td><?php echo "{$row['location']}" ?></td>
+                <td><?php echo "{$row['sessionID']}"; ?></td>
+                <td><?php echo "{$row['topic']}" ;?></td>
+                <td><?php echo "{$row['subjectCode']}"; ?></td>
+                <td><?php echo "{$row['date']}"; ?></td>
+                <td><?php echo "{$row['startTime']}"; ?></td>
+                <td><?php echo "{$row['endTime']}"; ?></td>
+                <td><?php echo "$durationd"; ?></td>
+                <td><?php echo "{$row['location']}"; ?></td>
+                <td><?php echo "{$row2['noOfStudent']}"; ?></td>
                 <td>
                     <input type="text" name="sessionID" value="<?php echo $row['sessionID']; ?>" style="display:none">
                     <input type="text" name="topic" value="<?php echo $row['topic']; ?>" style="display:none">
@@ -58,14 +59,17 @@ $result = mysqli_query($dbc, $query) or die("Query Failed $query");
                     <input type="text" name="endTime" value="<?php echo $row['endTime']; ?>" style="display:none">
                     <input type="text" name="duration" value="<?php echo $durationd; ?>" style="display:none">
                     <input type="text" name="location" value="<?php echo $row['location']; ?>" style="display:none">
+
+                <?php if (($row2['noOfStudent'])>=1){?>
                     <input type="submit" name="showStudentList" value="Show Student List">
+                <?php } else {echo "No student available to show";}?>
                 </td>
             </form>
     </tr>
         <?php
         }
         ?>
-</table>
+</table><br>
 
 <form method='POST' action='tutUI.php'>
     <input type="submit" value="Back to Tutor UI">
--- tutShowStudents.php
+++ tutShowStudents.php
@@ -4,5 +4,37 @@ SESSION_START();
 //$_SESSION['userClass'] = "STU";
 $dbc = mysqli_connect('localhost', 'root', '', 'utem_student_tutor_system') or die("Connection not established");
 //$_SESSION['loginUser'];
+$index=1;
 ?>
+<h3>Student List for topic: <?php echo " {$_POST['topic']}";?> (<?php echo "{$_POST['sessionID']}";?>) </h3><br>
 
+<table border='1'>
+    <tr>
+        <th>No.</th>
+        <th>Student ID</th>
+        <th>Student Name</th>
+    </tr>
+
+    <tr>
+
+    <?php
+        $query="SELECT t.studentID,s.name FROM student s, session_student t WHERE s.studentID=t.studentID AND t.sessionID='{$_POST['sessionID']}';";
+        $result=mysqli_query($dbc,$query)or die("Query Failed $query");
+        while($row=mysqli_fetch_assoc($result))
+        { ?>
+            <td>
+                <?php 
+                    echo"$index";
+                    $index++;
+                ?>
+            </td>
+            <td><?php echo"{$row['studentID']}"; ?></td>
+            <td><?php echo"{$row['name']}"; ?></td>
+        </tr>
+    <?php    
+        }
+    ?>
+</table> <br>
+<form method='POST' action='tutShowSession.php'>
+    <input type="submit" value="Return to Show Session page">
+</form>
