e6b0ecb8df729afd4db87d4e7ac6ec5082e3c31d
Janson-L <52344189+Janson-L@users.noreply.github.com>
2019-11-27 21:12:23 +0800

    Update admManageUsers.php

--- admManageUsers.php
+++ admManageUsers.php
@@ -1,23 +1,23 @@
 <?php
 SESSION_START();
 if (preg_match("/ADM/", @$_SESSION['loginUser'])) {
-?>
-<?php
-    $dbc = mysqli_connect('localhost', 'root', '', 'utem_student_tutor_system') or die("Connection not established");
-    $searchType = "";
-    $searchQuery = "";
-    $searchTable = 0;
+    ?>
+    <?php
+        $dbc = mysqli_connect('localhost', 'root', '', 'utem_student_tutor_system') or die("Connection not established");
+        $searchType = "";
+        $searchQuery = "";
+        $searchTable = 0;
 
-    if (isset($_POST['searchType'])) {
-        $searchType = $_POST['searchType'];
-    }
+        if (isset($_POST['searchType'])) {
+            $searchType = $_POST['searchType'];
+        }
 
-    if (isset($_POST['searchQuery'])) {
-        $searchQuery = $_POST['searchQuery'];
-    }
-?>
-<h2>Delete User</h2>
-<form method='POST'>
+        if (isset($_POST['searchQuery'])) {
+            $searchQuery = $_POST['searchQuery'];
+        }
+        ?>
+    <h2>Manage Users</h2>
+    <form method='POST'>
         <label>Search Type</label>
         <select name='searchType' required>
             <option <?php if ($searchType == "userIDSearch") echo 'selected="selected"'; ?>value='userIDSearch'>Search by userID</option>
@@ -29,61 +29,64 @@ if (preg_match("/ADM/", @$_SESSION['loginUser'])) {
     </form>
     <form method='POST' action='admManageUsers.php'>
         <input type='submit' value='Refresh'><br>
-</form>
+    </form>
 
     <?php
         if (isset($_POST['search'])) {
             if ($_POST['searchType'] == "userIDSearch") {
                 $searchTable = 1;
-            } 
-            else if ($_POST['searchType'] == "userNameSearch") {
+            } else if ($_POST['searchType'] == "userNameSearch") {
                 $searchTable = 2;
             }
         }
-    ?>
+        ?>
 
-    <?php if ($searchTable == 0) { ?>
-        <h3>Show All</h3>
-     <?php }    
-    else if ($searchTable == 1) { ?>
+    <?php
+        if ($searchTable == 1) { ?>
         <h3>Search by User ID</h3>
-    <?php } 
-    else if ($searchTable == 2) { ?>
+    <?php } else if ($searchTable == 2) { ?>
         <h3>Search by User Name</h3>
     <?php } ?>
-    <table border='1'>
-        <tr>
-            <th>User ID</th>
-            <th>Name</th>
-            <th>Matrix No</th>
-            <th>Phone No</th>
-            <th>Login Attempt</th>
-            <th>Account Status</th>
-            <th>Edit</th>
-            <th>Reset Password</th>
-            <th>Delete</th>
-        </tr>
-        <?php
-            if($searchTable==0)
-            {
-                $query="SELECT studentID AS userID, name, matrixNo, phoneNo,loginAttempt, accountStatus FROM student UNION
-                SELECT tutorID, name, matrixNo, phoneNo,loginAttempt, accountStatus FROM tutor;";
-            }
-            if($searchTable==1)
-            {
-                $query="SELECT studentID, name, matrixNo, phoneNo,loginAttempt, accountStatus FROM student UNION
-                SELECT tutorID, name, matrixNo, phoneNo,loginAttempt, accountStatus FROM tutor WHERE userID='$searchQuery';";
-            }
-            else if($searchTable==2)
-            {
-                $query="SELECT studentID, name, matrixNo, phoneNo,loginAttempt, accountStatus FROM student UNION
+    <?php
+        // if($searchTable==0)
+        // {
+        //     $query="SELECT studentID AS userID, name, matrixNo, phoneNo,loginAttempt, accountStatus FROM student UNION
+        //     SELECT tutorID, name, matrixNo, phoneNo,loginAttempt, accountStatus FROM tutor;";
+        // }
+        if (isset($_POST['search'])) {
+            if ($searchTable == 1) {
+                $query = "SELECT studentID AS userID, name, matrixNo, phoneNo,loginAttempt, accountStatus FROM student WHERE studentID='$searchQuery' UNION
+                SELECT tutorID, name, matrixNo, phoneNo,loginAttempt, accountStatus FROM tutor WHERE tutorID='$searchQuery';";
+            } else if ($searchTable == 2) {
+                $query = "SELECT studentID AS userID, name, matrixNo, phoneNo,loginAttempt, accountStatus FROM student WHERE name LIKE'%$searchQuery%' UNION
                 SELECT tutorID, name, matrixNo, phoneNo,loginAttempt, accountStatus FROM tutor WHERE name LIKE'%$searchQuery%';";
             }
-            $result = mysqli_query($dbc, $query) or die("Query Failed $query");
 
-            while ($row = mysqli_fetch_assoc($result)) {
-                ?>
+            $result = mysqli_query($dbc, $query) or die("Query Failed $query");
+            if(mysqli_num_rows($result)>0){
+            ?> 
+            <table border='1'>
             <tr>
+                <th>User ID</th>
+                <th>Name</th>
+                <th>Matrix No</th>
+                <th>Phone No</th>
+                <th>Login Attempt</th>
+                <th>Account Status</th>
+                <th>Edit</th>
+                <th>Reset Password</th>
+                <th>Delete</th>
+            </tr>
+            <?php
+            }
+            else
+            {
+                echo"No result is found. Please make sure you have the correct search term.";
+            }
+
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        ?>
+                <tr>
                     <td><?php echo $row['userID']; ?></td>
                     <td><?php echo $row['name']; ?></td>
                     <td><?php echo $row['matrixNo']; ?></td>
@@ -91,15 +94,15 @@ if (preg_match("/ADM/", @$_SESSION['loginUser'])) {
                     <td><?php echo $row['loginAttempt']; ?></td>
                     <td><?php echo $row['accountStatus']; ?></td>
                     <td>
-                    <form method='POST' action='admEditUser.php'>
-                        <input type="text" name="userID" value="<?php echo $row['userID']; ?>" style="display:none">
-                        <input type="text" name="name" value="<?php echo $row['name']; ?>" style="display:none">
-                        <input type="text" name="matrixNo" value="<?php echo $row['matrixNo']; ?>" style="display:none">
-                        <input type="text" name="phoneNo" value="<?php echo $row['phoneNo']; ?>" style="display:none">
-                        <input type="text" name="loginAttempt" value="<?php echo $row['loginAttempt']; ?>" style="display:none">
-                        <input type="text" name="accountStatus" value="<?php echo $row['accountStatus']; ?>" style="display:none">     
-                        <input type="submit" name="editUser" value="Edit User"> 
-                    </form>       
+                        <form method='POST' action='admEditUser.php'>
+                            <input type="text" name="userID" value="<?php echo $row['userID']; ?>" style="display:none">
+                            <input type="text" name="name" value="<?php echo $row['name']; ?>" style="display:none">
+                            <input type="text" name="matrixNo" value="<?php echo $row['matrixNo']; ?>" style="display:none">
+                            <input type="text" name="phoneNo" value="<?php echo $row['phoneNo']; ?>" style="display:none">
+                            <input type="text" name="loginAttempt" value="<?php echo $row['loginAttempt']; ?>" style="display:none">
+                            <input type="text" name="accountStatus" value="<?php echo $row['accountStatus']; ?>" style="display:none">
+                            <input type="submit" name="editUser" value="Edit User">
+                        </form>
                     </td>
                     <td>
                         <form method='POST' action='admResetPassword.php'>
@@ -109,44 +112,44 @@ if (preg_match("/ADM/", @$_SESSION['loginUser'])) {
                             <input type="text" name="phoneNo" value="<?php echo $row['phoneNo']; ?>" style="display:none">
                             <input type="text" name="loginAttempt" value="<?php echo $row['loginAttempt']; ?>" style="display:none">
                             <input type="text" name="accountStatus" value="<?php echo $row['accountStatus']; ?>" style="display:none">
-                            <input type="submit" name="resetPassword" value="Reset Password">   
-                        </form>     
+                            <input type="submit" name="resetPassword" value="Reset Password">
+                        </form>
                     </td>
                     <td>
-                    <form method='POST' action='admDeleteUser.php'>
-                        <input type="text" name="userID" value="<?php echo $row['userID']; ?>" style="display:none">
-                        <input type="text" name="name" value="<?php echo $row['name']; ?>" style="display:none">
-                        <input type="text" name="matrixNo" value="<?php echo $row['matrixNo']; ?>" style="display:none">
-                        <input type="text" name="phoneNo" value="<?php echo $row['phoneNo']; ?>" style="display:none">
-                        <input type="text" name="loginAttempt" value="<?php echo $row['loginAttempt']; ?>" style="display:none">
-                        <input type="text" name="accountStatus" value="<?php echo $row['accountStatus']; ?>" style="display:none">
-                        <input type="submit" name="deleteUser" value="Delete User">
-                    </form>
+                        <form method='POST' action='admDeleteUser.php'>
+                            <input type="text" name="userID" value="<?php echo $row['userID']; ?>" style="display:none">
+                            <input type="text" name="name" value="<?php echo $row['name']; ?>" style="display:none">
+                            <input type="text" name="matrixNo" value="<?php echo $row['matrixNo']; ?>" style="display:none">
+                            <input type="text" name="phoneNo" value="<?php echo $row['phoneNo']; ?>" style="display:none">
+                            <input type="text" name="loginAttempt" value="<?php echo $row['loginAttempt']; ?>" style="display:none">
+                            <input type="text" name="accountStatus" value="<?php echo $row['accountStatus']; ?>" style="display:none">
+                            <input type="submit" name="deleteUser" value="Delete User">
+                        </form>
                     </td>
-            </tr>
-            <?php } ?>
-            </table> <br>
- 
+                </tr>
+        <?php }
+            } ?>
+        </table> <br>
 
 
-<form method='POST' action='admUI.php'>
-<input type="submit" value="Back to Admin UI">
-</form>
 
-<?php
-        if (isset($_POST['deleteUser'])) {
-            $query = "INSERT INTO session_student (sessionID,studentID) VALUES('{$_POST['sessionID']}','{$_SESSION['loginUser']}');";
-            $result = mysqli_query($dbc, $query) or die("Query Failed $query");
-            echo '<meta http-equiv="refresh" content="0">';
-            die();
-        }
-?>
+        <form method='POST' action='admUI.php'>
+            <input type="submit" value="Back to Admin UI">
+        </form>
 
-<?php
-}
-else {
-    echo "<h3>You don't have the privilege to view this page. You will be logged out and redirected to the login page in 5 seconds.<br> Please login with the correct account.</h3>";
-    header("Refresh:5;URL=logOut.php");
-   die();
-}
-?>
+        <?php
+            if (isset($_POST['deleteUser'])) {
+                $query = "INSERT INTO session_student (sessionID,studentID) VALUES('{$_POST['sessionID']}','{$_SESSION['loginUser']}');";
+                $result = mysqli_query($dbc, $query) or die("Query Failed $query");
+                echo '<meta http-equiv="refresh" content="0">';
+                die();
+            }
+            ?>
+
+    <?php
+    } else {
+        echo "<h3>You don't have the privilege to view this page. You will be logged out and redirected to the login page in 5 seconds.<br> Please login with the correct account.</h3>";
+        header("Refresh:5;URL=logOut.php");
+        die();
+    }
+    ?>
